360ad7da0bd8b5ceb7a7fa815bccdec78bc35359
Dan Hixon <danhixon@gmail.com>
2012-10-20 22:16:50 -0700

    added support for IntegerArray

---
+++ lib/dm-pg-types/integer_array.rb
@@ -0,0 +1,15 @@
+require 'dm-core'
+
+module DataMapper
+  class Property
+    class IntegerArray < PgArray
+      def initialize(model, name, options = {})
+        super
+      end
+
+      def load(value)
+        super.map(&:to_f) if value
+      end
+    end # class IntegerArray
+  end # class Property
+end # module DataMapper
